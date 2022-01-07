e9ab021b8f14e9ffdaab04edb710ccf3cd60cac8
ibrahim Turancı <ibrahim-turanci@hotmail.com>
2022-01-07 22:56:19 +0300

    initial commit

---
+++ ibrahimTuranci_Odev2_GarbageCollector/Controllers/ContainerController.cs
@@ -0,0 +1,64 @@
+﻿using Data.DataModels;
+using Data.UoW;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ibrahimTuranci_Odev2_GarbageCollector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContainerController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ContainerController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allContainers = await unitOfWork.Containers.GetAll();
+            return Ok(allContainers);
+        }
+        [HttpPost]
+        public ActionResult<Container> Post([FromBody] Container request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+
+            unitOfWork.Containers.Add(request);
+            unitOfWork.Complete();
+            return request;
+        }
+        [HttpPut]
+        public ActionResult<Container> Put([FromBody] Container request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+           Container oldcontainer =  unitOfWork.Containers.Get(request.Id);
+            unitOfWork.Containers.Detach(oldcontainer);
+            if (oldcontainer.VehicleId != request.VehicleId)
+            {
+                return BadRequest("VehicleId Değiştirilemez");
+            }
+
+            unitOfWork.Containers.Update(request);
+            unitOfWork.Complete();
+            return request;
+        }
+        [HttpDelete("id")]
+        public int Delete(int id)
+        {
+
+
+            unitOfWork.Containers.Delete(id);
+            unitOfWork.Complete();
+            return id;
+        }
+    }
+}
---
+++ ibrahimTuranci_Odev2_GarbageCollector/Controllers/VehicleController.cs
@@ -0,0 +1,69 @@
+﻿using Data.DataModels;
+using Data.UoW;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ibrahimTuranci_Odev2_GarbageCollector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public VehicleController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var allVehicles = await unitOfWork.Vehicles.GetAll();
+            return Ok(allVehicles);
+
+        } 
+        [HttpGet]    //
+        [Route("Containers")]
+        public async Task<IActionResult> GetContainersOfVehicle([FromQuery]long Id)    
+        {
+            
+            var allContainers = await unitOfWork.Containers.GetAll();
+            return Ok(allContainers.Where(x => x.VehicleId == Id));
+        }
+        [HttpPost]
+        public ActionResult<Vehicle> Post([FromBody] Vehicle request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+
+            unitOfWork.Vehicles.Add(request);
+            unitOfWork.Complete();
+            return request;
+        }
+        [HttpPut]
+        public ActionResult<Vehicle> Put([FromBody] Vehicle request)
+        {
+            if (request == null)
+            {
+                return BadRequest();
+            }
+
+            unitOfWork.Vehicles.Update(request);
+            unitOfWork.Complete();
+            return request;
+        }
+        [HttpDelete("id")]
+        public int Delete(int id)
+        {
+
+
+            unitOfWork.Vehicles.Delete(id);
+            unitOfWork.Complete();
+            return id;
+        }
+    }
+
+}
